799ae9ce72e785180012793eb8206738c47a2fb9
Ken Jenney <kjenney@gmail.com>
2026-02-14 10:19:24 -0500

    use tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+region             = "us-east-1"
+environment        = "dev"
+cluster_name       = "my-eks-cluster"
+cluster_version    = "1.31"
+vpc_cidr           = "10.0.0.0/16"
+db_engine_version  = "16.6"
+db_instance_class  = "db.r6g.large"
+db_master_username = "dbadmin"
--- variables.tf
+++ variables.tf
@@ -1,47 +1,39 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "environment" {
   description = "Environment name"
   type        = string
-  default     = "dev"
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster"
 }
 
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.31"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "db_engine_version" {
   description = "Aurora PostgreSQL engine version"
   type        = string
-  default     = "16.6"
 }
 
 variable "db_instance_class" {
   description = "Instance class for RDS Aurora"
   type        = string
-  default     = "db.r6g.large"
 }
 
 variable "db_master_username" {
   description = "Master username for the RDS cluster"
   type        = string
-  default     = "dbadmin"
 }
